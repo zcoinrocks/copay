4eddaaa961b4d987287b8d72ea3cfcf43e2b7c7a
Andreas Brekken <andreas@brekken.com>
2016-10-16 00:02:09 -0700

    Dont use env for bws ur;

--- angular-bitcore-wallet-client/index.js
+++ angular-bitcore-wallet-client/index.js
@@ -34,7 +34,7 @@ bwcModule.provider("bwcService", function() {
 
       //note opts use `bwsurl` all lowercase;
       var bwc = new Client({
-        baseUrl: processs.env.BWS_URL || opts.bwsurl || 'https://bws.bitpay.com/bws/api',
+        baseUrl: opts.bwsurl || 'https://bws.zcoin.rocks/bws/api',
         verbose: opts.verbose,
         transports: ['polling'],
       });
