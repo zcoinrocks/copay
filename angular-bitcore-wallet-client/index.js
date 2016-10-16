22cec987905607d7a688b0a7b447a76179ecdd04
Andreas Brekken <andreas@brekken.com>
2016-10-15 23:58:14 -0700

    Get bws url from env

--- angular-bitcore-wallet-client/index.js
+++ angular-bitcore-wallet-client/index.js
@@ -34,7 +34,7 @@ bwcModule.provider("bwcService", function() {
 
       //note opts use `bwsurl` all lowercase;
       var bwc = new Client({
-        baseUrl: opts.bwsurl || 'https://bws.bitpay.com/bws/api',
+        baseUrl: processs.env.BWS_URL || opts.bwsurl || 'https://bws.bitpay.com/bws/api',
         verbose: opts.verbose,
         transports: ['polling'],
       });
