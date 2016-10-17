f105d1a8441b44eaf603e1e61be63fb3e1606ee0
Andreas Brekken <andreas@brekken.com>
2016-10-17 16:16:01 -0700

    Fix branding

--- src/js/controllers/preferencesHistory.js
+++ src/js/controllers/preferencesHistory.js
@@ -81,7 +81,7 @@ angular.module('copayApp.controllers').controller('preferencesHistory',
             var _fee = (it.fees * satToBtc).toFixed(8)
             $scope.csvContent.push({
               'Date': formatDate(it.time * 1000),
-              'Destination': 'Bitcoin Network Fees',
+              'Destination': 'ZCoin Network Fees',
               'Description': '',
               'Amount': '-' + _fee,
               'Currency': 'BTC',
